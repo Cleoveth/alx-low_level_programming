56c1c0f9fb6e74d31d3f64a4bd054ab7cc97a163
Cleoveth <lovettonyia@gmail.com>
2023-08-28 02:36:43 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,8 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _sqrt(int n, int p);
 int is_prime(int n, int num);
-
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
 #endif
