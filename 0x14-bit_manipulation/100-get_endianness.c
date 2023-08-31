10acb51431be40360f0f7d77655a2d56ba41d0ef
Cleoveth <lovettonyia@gmail.com>
2023-08-31 16:23:10 -0700

    Bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness of the system.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *endian_check = (char *)&num;
+
+	/* If the first byte is 1, the system is little endian */
+	return (*endian_check);
+}
+
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,10 +10,10 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8) /* Check if index is valid */
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	*n &= ~(1 << index); /* Use bitwise AND with the complement to clear the bit */
+	*n &= ~(1 << index);
 	return (1);
 }
 
